38229fa77f70fe27ee07b036aadbb8d4e85a4a03
Felipe Todeschini <todeschini@hotmail.com>
2024-08-08 23:01:43 -0400

    Adjustment of the padding style in the modal white outer container

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,7 +3,7 @@ import '../css/modal.css';
 export default function Modal({modalId, padding, children}){
     return (
         <div id={modalId} className='modal'>
-                <div className="modal-container" style={{padding: padding}}>
+                <div className="modal-container" style={{padding: `${padding}rem`}}>
                     <a href="#" className="modal-close">&times;</a>
                     { children }
                 </div>
