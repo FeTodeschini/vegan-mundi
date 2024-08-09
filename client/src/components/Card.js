c312da5a1b35c413fc7a7a73c91a43c406dc1fa7
Felipe Todeschini <todeschini@hotmail.com>
2024-08-08 23:29:49 -0400

    Composition of Card component - CardTopImage and CardContent

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,60 +1,35 @@
-import { Link } from 'react-router-dom';
-import iconPlay from '../assets/icon-play.svg';
+import CardTopImage from './CardTopImage';
+import CardContent from './CardContent';
 
 import '../css/card.css';
 
 export default function Card({ imgSource, imgLink, linkState, isVideo, title, description, descriptionList, bgColor, callOutTag, children}){
-    
     // Creates an object for applying a background color style in case a bgColor was received by the Card 
     const style = bgColor !== undefined ? {backgroundColor: bgColor} : {};
 
+    // Think of a better implementation for avoiding prop drilling
     return (
         <div className="card" style={style} key={title}>
-            
-            {/* 1-Image on the top of the card (it can be either an image or a video) */}
             { imgSource !== undefined &&
-            
-                <div className="card__img-wrapper">
-                    <Link to={imgLink} state={linkState}><img className="img-medium" src={imgSource}></img></Link>
-                    {/* displays the play button for playing the video in case the image of the card is a link to a video ( playVideo=true) */}
-                    {isVideo && <Link to={imgLink} state={linkState}><img className='icon-play' src={iconPlay}></img></Link>}
-                </div>
+                <CardTopImage 
+                    isVideo={isVideo}
+                    imgLink={imgLink}
+                    linkState={linkState}
+                    imgSource={imgSource}
+                />
             }
 
-            {/* 2-Card text content - Usually a title and a description*/}
             <div className="card__content">
+                <CardContent 
+                    callOutTag={callOutTag}
+                    title={title}
+                    description={description}
+                    descriptionList={descriptionList}
+                />
 
-                {/* JSX for card content when there is no call out tag */}
-                {callOutTag === undefined && 
-                    <>
-                        {/* <p className='card__title'>{titleIcon && <img className="img-small" src={titleIcon} />} {title}</p> */}
-                        <p className='card__title'>{title}</p>
-                        <p className='card__description'>{description}</p>
-                        <div className="card__description-list">
-                            { descriptionList }
-                        </div>
-                    </>
-                }
-                
-                {/* JSX for card content when there is a call out tag */}
-                {callOutTag !== undefined && 
-                    <>
-                        <div className="card__call-out-container">
-                            <p className='card__title'>{title}</p>
-                            <p className="card__call-out-tag">{callOutTag}</p>
-                        </div>
-                        
-                        <p className="card__description">{description}</p>
-
-                        { descriptionList }
-                    </>
-                }
-
-                {/* 3-Any additional content passed by the parent component */}
+                {/* Any additional content passed by the parent component */}
                 {children}  
-
             </div>
-
         </div>
     )
 }
