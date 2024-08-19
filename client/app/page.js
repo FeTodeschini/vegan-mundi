39f7f2f0bb92a2a24c07cf5817f34683e370021b
Felipe Todeschini <todeschini@hotmail.com>
2024-08-19 19:18:00 -0400

    Issue #8-Styles are messed up when page is mnually refreshed

---
+++ client/app/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import LandingPage from './_components/LandingPage';
+import DarkBackground from './_components/DarkBackground.js';
+
+export default function App() {
+
+  return (
+    <>
+        <DarkBackground />
+        <LandingPage />
+     </>
+  );
+}
