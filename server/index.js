ff276332d0140ab95e583bd4fcf02725dc328638
Felipe Todeschini <todeschini@hotmail.com>
2024-09-26 01:36:10 -0400

    Jenkins pipeline test

--- server/index.js
+++ server/index.js
@@ -140,7 +140,7 @@ app.get('/thumbnails/:thumbnail', async (req, res) => {
 // }
 
 app.get('/makebah', async (req, res) => {
-  res.send("makebhostonaa")
+  res.send("makebarbaridade")
 })
 
 // GET ALL FREE CLASSES (VEGAN-MUNDI.CLASSES.CATEGORY_ID = 1)
