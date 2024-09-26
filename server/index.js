5aaa885f42af2796cc24bd58dd3717c8ef35dca9
Felipe Todeschini <todeschini@hotmail.com>
2024-09-26 01:46:12 -0400

    Jenkins pipeline test

--- server/index.js
+++ server/index.js
@@ -139,10 +139,6 @@ app.get('/thumbnails/:thumbnail', async (req, res) => {
 
 // }
 
-app.get('/makebah', async (req, res) => {
-  res.send("makebarbaridademexmo")
-})
-
 // GET ALL FREE CLASSES (VEGAN-MUNDI.CLASSES.CATEGORY_ID = 1)
 app.get('/classes/free', async (req, res) => {
     const dbConnection = await connectToDb();
