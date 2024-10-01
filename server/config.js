2101243fe202fc56476ebf14889f18a553a3616c
Felipe Todeschini <todeschini@hotmail.com>
2024-10-01 17:23:42 -0400

    Added .trim() to variables to prevent \r error in the Ubuntu server while reading .env

--- server/config.js
+++ server/config.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-    dbHost: process.env.DB_ENDPOINT,
-    dbName: process.env.DB_NAME,
-    dbPort: process.env.DB_PORT,
-    dbUserName: process.env.DB_USER,
-    dbSecret: process.env.DB_SECRET,
-
-    awsRegion: process.env.AWS_REGION,
-    awsAccessKey: process.env.AWS_ACCESS_KEY,
-    awsSecretKey: process.env.AWS_SECRET_KEY,
-
-    jwtSecretKey: process.env.JWT_SECRET_KEY
+    dbHost: process.env.DB_ENDPOINT?.trim(),
+    dbName: process.env.DB_NAME?.trim(),
+    dbPort: process.env.DB_PORT?.trim(),
+    dbUserName: process.env.DB_USER?.trim(),
+    dbSecret: process.env.DB_SECRET?.trim(),
+    awsRegion: process.env.AWS_REGION?.trim(),
+    jwtSecretKey: process.env.JWT_SECRET_KEY?.trim()
 }
