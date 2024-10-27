fdceda4143d63da88de44bc7c7eeeabd5b4782db
Felipe Todeschini <todeschini@hotmail.com>
2024-10-27 15:38:00 -0400

    Issue #39-Search not returning results for signed user

--- server/controllers/classController.js
+++ server/controllers/classController.js
@@ -124,7 +124,7 @@ async function getClassesPerKeyword (req, res, next) {
 
     try {
         connection = await connectToDb();
-        let query = `SELECT DISTINCT CLS.CATEGORY_ID, CLS.TITLE, CLS.DESCRIPTION, CLS.PHOTO, ` +
+        let query = `SELECT DISTINCT CLS.CATEGORY_ID, CLS.CLASS_ID, CLS.TITLE, CLS.TITLE, CLS.DESCRIPTION, CLS.PHOTO, ` +
           ` ( SELECT GROUP_CONCAT(TITLE SEPARATOR '|') 
               FROM 
               RECIPE R 
@@ -133,21 +133,19 @@ async function getClassesPerKeyword (req, res, next) {
                   AND C.CLASS_ID = CLS.CLASS_ID
                   AND (SELECT COUNT(TITLE) FROM RECIPE R INNER JOIN CLASS_RECIPE C ON R.RECIPE_ID = C.RECIPE_ID AND C.CLASS_ID = CLS.CLASS_ID ) > 1
               ) CLASSES_LIST ` + 
-           ` FROM ` +
-           ` CLASS CLS `
+           ` FROM CLASS CLS ` +
+            ` INNER JOIN CLASS_RECIPE CLR ON ` +
+            `   CLS.CLASS_ID = CLR.CLASS_ID ` 
 
-        if (email !== null) {
-            query +=   `AND CLS.CLASS_ID NOT IN (
-                            SELECT CLASS_ID FROM ORDER_CLASS OCL WHERE EMAIL = ?)`
-        }
-
-        query += ` INNER JOIN CLASS_RECIPE CLR ON ` +
-            `   CLS.CLASS_ID = CLR.CLASS_ID ` +
-            ` INNER JOIN RECIPE RCP ON ` +
+            if (email !== null) {
+                query +=   ` AND CLS.CLASS_ID NOT IN (
+                                SELECT CLASS_ID FROM ORDER_CLASS OCL WHERE EMAIL = ?) `
+            }
+            
+            query += ` INNER JOIN RECIPE RCP ON ` +
             `   CLR.RECIPE_ID = RCP.RECIPE_ID ` +
             `   AND RCP.KEYWORD LIKE ?`;
-        
-            console.log(query)
+
         // result needs to be destructured as mysql2/promise returns 2 items: the rows themselves plus metadata about the result
         const [result] = await connection.query(query, params);
         res.send(result);
