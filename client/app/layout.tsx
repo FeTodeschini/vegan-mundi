8c2dacddbdcbdbe0b4d1fadee4d569b805f6d802
Felipe Todeschini <todeschini@hotmail.com>
2024-11-08 03:23:43 -0500

    Issue #26-Implement Redux for the shopping cart state management

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -27,15 +27,13 @@ export default function Layout({ children }: ChildrenProps){
         </head>
         <body>
           <StateProvider>
+            <Provider store={store}>
               {/* Conditional rendering of the Header needed to be implemented as the App Router hirerachical structure with different layouts
               doesn't work properly with client side components */}
-              {!excludeHeaderRoutes.includes(route) && 
-                  <Provider store={store}>
-                    <Header/>
-                  </Provider>
-              }
+              {!excludeHeaderRoutes.includes(route) && <Header/>}
               <DarkBackground />
               {children}
+            </Provider>
           </StateProvider>
         </body>
       </html>
--- client/app/search/page.tsx
+++ client/app/search/page.tsx
@@ -5,8 +5,6 @@ import { useSearchParams } from 'next/navigation';
 import { Suspense, useState, useEffect, useContext } from "react";
 import { StateContext } from "../StateProvider";
 import { useAddPreSignedUrlToArray } from "../hooks/useAddPreSignedUrlToArray";
-import { Provider } from "react-redux";
-import store from "@/redux/store";
 import FilteredClasses from "../_components/FilteredClasses";
 import config from "../_lib/config";
 import { CookingClass } from '@/_types/cooking-class'
@@ -65,9 +63,7 @@ function SearchResultSuspense(){
     }
     else {
         return (
-            <Provider store={store}>
-                <FilteredClasses images={images} resultsFound={results} title={`${results} result${results !== 1 ? "s" : "" } found for your search`} subTitle={`Keyword: ${keyword}`}/>            
-            </Provider>
+            <FilteredClasses images={images} resultsFound={results} title={`${results} result${results !== 1 ? "s" : "" } found for your search`} subTitle={`Keyword: ${keyword}`}/>            
         )        
     }
 
