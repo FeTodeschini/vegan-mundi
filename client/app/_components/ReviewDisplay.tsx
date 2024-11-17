ca72b4d9a712d5c1b403aeeffde6ab01e9f3eb06
Felipe Todeschini <todeschini@hotmail.com>
2024-11-17 14:02:47 -0500

    Issue #53-Add review stars to searched cooking classes

--- client/app/_components/ReviewDisplay.tsx
+++ client/app/_components/ReviewDisplay.tsx
@@ -1,24 +1,16 @@
-import StarFull from "./StarFull";
-import StarEmpty from "./StarEmpty";
+import ReviewStarsContainer from "./ReviewStarsContainer";
 import ExpandableText from "./ExpandableText";
 
-export default function ReviewDisplay({stars, reviewText} : {stars: number | undefined, reviewText: string | undefined}) {
+export default function ReviewDisplay({stars, reviewText, isReviewed=false}: 
+        {stars: number | undefined, reviewText: string | undefined, isReviewed: boolean}) {
     return (
         <div>
-            <div className="review__container">
-                <div className="review__star-container">
-                    {Array.from({ length: 5 }, (_, i) => (
-                        <span className="review__star-display">
-                            {(i + 1 <= stars!) ? <StarFull/> : <StarEmpty/>}
-                        </span>
-                    ))}
-                </div>
-            <p className="review__star-text">{stars}</p>
-            </div>
-            
-            <ExpandableText labelShowMore={"Show Review"} labelShowLess={"Hide Review"}>
+            <ReviewStarsContainer stars={stars} isReviewed={isReviewed} />
+            {reviewText!=="" && 
+                <ExpandableText labelShowMore={"Show Review"} labelShowLess={"Hide Review"}>
                     {reviewText}
-            </ExpandableText>
+                </ExpandableText>
+            }
         </div>
     )
 }
