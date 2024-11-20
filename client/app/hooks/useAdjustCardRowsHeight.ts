42550e25b2b544d1de352cb8572d6ea06df1a231
Felipe Todeschini <todeschini@hotmail.com>
2024-11-19 19:50:43 -0500

    Issue #13-Adjusted Card responsive layout

---
+++ client/app/hooks/useAdjustCardRowsHeight.ts
@@ -0,0 +1,70 @@
+import { useEffect, useRef } from "react";
+
+// Adjust the height of the rows of all cards in a specific page. The baseline for the adjustment is the height of the row with more content
+// The logic is based on the classes (css) applied to the <Card/> component
+// Only the rows with the class .card-content will be adjusted
+export default function useAdjustCardRowsHeight(dependencyArray) {
+    // Creates a ref to the card container so it can be manipulated in the DOM
+    const containerRef = useRef();
+    const cardSelector = ".card";
+    const rowSelector = ".card__section";
+
+    useEffect(() => {
+        // Makes sure that the hook is only executed when all the data is fetched. This is needed because the hook can be called from children compoents
+        // that relies on  the parent's data
+        if (!dependencyArray[0]) {
+            return;
+        }
+
+        function adjustCardRowsHeight() {
+            const container = containerRef.current;
+            if (!container) return;
+
+            // Loop through all cards and get all the rows with class .card__content for each card
+            const cards = Array.from(container.querySelectorAll(cardSelector));
+            if (cards.length === 0) {
+                console.log("No cards found, retrying...");
+                setTimeout(adjustCardRowsHeight, 100);
+                return;
+            }
+
+            const rowGroups = [];
+
+            //  Groups all rows in the same position (i.e.: all 1st rows, all 2nd rows and so on)
+            cards.forEach((card) => {
+                const rows = Array.from(card.querySelectorAll(rowSelector));
+                rows.forEach((row, rowIndex) => {
+                    if (!rowGroups[rowIndex]) rowGroups[rowIndex] = [];
+                    rowGroups[rowIndex].push(row);
+                });
+            });
+
+            // Reset heights to auto before recalculating
+            Object.values(rowGroups).forEach((group) =>
+                group.forEach((row) => (row.style.height = "auto"))
+            );
+
+            // For each group of rows (1st, 2nd, 3rd), determine the row with max height and then set this height to all rows of the group
+            Object.values(rowGroups).forEach((group) => {
+                const maxHeight = Math.max(...group.map((row) => row.offsetHeight));
+                console.log(`Max height for group: ${maxHeight}`);
+                group.forEach((row) => {
+                    row.style.height = `${maxHeight}px`;
+                });
+            });
+        }
+
+        adjustCardRowsHeight();
+
+        // makes sure that the resize function is called in case the browser is resized
+        window.addEventListener("resize", adjustCardRowsHeight);
+
+        // Cleanup function for removing the event listener when the component is unmounted
+        return () => window.removeEventListener("resize", adjustCardRowsHeight);
+    }, [dependencyArray]);
+
+    return containerRef; // Return the container ref for the component to attach
+}
+
+            
+
