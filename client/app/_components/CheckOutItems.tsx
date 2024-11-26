63146fdb3dabeb61a93ecb2dde4e84adac794515
Felipe Todeschini <todeschini@hotmail.com>
2024-11-25 23:52:34 -0500

    Issue #13-Review responsive layout

--- client/app/_components/CheckOutItems.tsx
+++ client/app/_components/CheckOutItems.tsx
@@ -5,6 +5,8 @@ import SectionHeader from './SectionHeader';
 import { SelectedCookingClassWithPrices } from '../_types/cooking-class';
 import { useSelector } from 'react-redux';
 import { ReduxRootState } from '../_types/redux';
+import '../_styles/form.css';
+import '../_styles/layout.css';
 
 export default function CheckoutItems() {
 
@@ -12,27 +14,32 @@ export default function CheckoutItems() {
 
     return (
         <>
-                <div className="grid-auto-fit">
-                    <div>
+                <div className="checkout-container">
+                    <div className="checkout__credit-card">
                         <SectionHeader subTitle="Credit card information"/>
-                        <div className="checkout regular-text">
-                            <form action="">
-
+                            <form action="" className="checkout__form regular-text">
+                                <label htmlFor="firstName" className='form-label'>First Name:</label>
+                                <input className="form-input" id="firstName" required/>
+                                <label htmlFor="lastName" className='form-label'>Last Name:</label>
+                                <input className="form-input" id="last" required/>
+                                <label htmlFor="cardNumber" className='form-label'>Card number:</label>
+                                <input className="form-input" id="cardNumber" required/>
                             </form>
-                        </div>
                     </div>
                     
-                    <div>
-                        <SectionHeader subTitle="Classes in your shopping cart"/>
-                        <div className="checkout__summary regular-text">
-                            {cartItems.map( item => 
-                                <div >
-                                    <p>{item.TITLE}</p>
-                                    <p className="regular-text-small">{item.PRICE_TYPE_DESCRIPTION} - ${item.PRICE}</p>
-                                </div>
-                            )}
-                            <p className="bold">Total: ${cartAmount}</p>
-                        </div>
+                    <div className="checkout__summary">
+                        <div>
+                            <SectionHeader subTitle="Classes in your shopping cart"/>
+                            <div className="checkout__summary--items regular-text">
+                                {cartItems.map( item => 
+                                    <div >
+                                        <p>{item.TITLE}</p>
+                                        <p className="regular-text-small">{item.PRICE_TYPE_DESCRIPTION} - ${item.PRICE}</p>
+                                    </div>
+                                )}
+                                <p className="bold">Total: ${cartAmount}</p>
+                            </div>
+                            </div>
                     </div>
                 </div>
         </>
