acb77e7f2ef73030185c25cacafce03b46f2643a
Felipe Todeschini <todeschini@hotmail.com>
2024-12-07 13:44:20 -0500

    Issue #74-Fix Jenkins pipeline for the client application

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,8 @@
+const path = require('path');
+
+console.log("Next.js config loaded");
+
 const nextConfig = {
-    // 7zoutput: 'export', // Outputs a Single-Page Application (SPA) as a Static Site.
-    // distDir: './dist', // Changes the build output directory to `./dist/`.
-    // missingSuspenseWithCSRBailout: false, // this is necessary to avoid the useSearchParams() in the search page to have to be wrapped in a Suspense Boundary
     reactStrictMode: true,
     disableOptimizedLoading: true,
     optimizeCss: true,
