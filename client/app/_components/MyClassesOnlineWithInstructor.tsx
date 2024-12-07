641c1fc0c319caaa72467433679f574bf6a49c23
Felipe Todeschini <todeschini@hotmail.com>
2024-12-07 11:27:04 -0500

    Issue #74-Fix Jenkins pipeline for the client application

--- client/app/_components/MyClassesOnlineWithInstructor.tsx
+++ client/app/_components/MyClassesOnlineWithInstructor.tsx
@@ -6,7 +6,7 @@ import CustomDatePicker from './CustomDatePicker';
 import VideoCallLink from './VideoCallLink';
 import ClassRescheduleDisclaimer from './ClassRescheduleDisclaimer';
 import { useSetClassDate } from '../hooks/useSetClassDate';
-import { handleSetMyClasstDate } from '../_lib/myClassesHelper';
+import { handleSetMyClasstDate } from '../_lib/lalamyClassesHelper';
 import { StateContext } from '../StateProvider';
 import ReviewDisplay from './ReviewDisplay';
 import { useSelector } from 'react-redux';
