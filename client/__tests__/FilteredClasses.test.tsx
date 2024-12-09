83507c1ed718d25a26d6835f0578bb6dce150b79
Felipe Todeschini <todeschini@hotmail.com>
2024-12-09 18:22:09 -0500

    Issue #67-Jest unit tests

---
+++ client/__tests__/FilteredClasses.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
+import FilteredClasses from '@/_components/FilteredClasses';
+
+test('receives keyword from SearchInput', ()=> {
+
+})
+
+test('receives category from ClassesCategory', ()=> {
+
+})
