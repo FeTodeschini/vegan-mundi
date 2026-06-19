d9d3de9080bfe8c22d74fa69366d85bd712cb652
Felipe Todeschini <phelipe.tod@gmail.com>
2026-06-19 13:19:04 -0400

    #107-Updated remaining import paths due to _components folder restructuring

--- client/app/review/[classId]/[classTitle]/page.tsx
+++ client/app/review/[classId]/[classTitle]/page.tsx
@@ -3,9 +3,9 @@
 import { useGetSectionDataWithParams } from '../../../hooks/useGetSectionDataWithParams';
 import { useMemo, useState } from 'react';
 import { useParams } from 'next/navigation';
-import SectionHeader from '../../../_components/SectionHeader';
-import Button from '../../../_components/Button';
-import Card from '../../../_components/Card';
+import SectionHeader from '../../../_components/common-ui/SectionHeader';
+import Button from '../../../_components/common-ui/Button';
+import Card from '../../../_components/card/Card';
 import { Review } from '../../../_types/review';
 import ReviewStarsContainer from '../../../_components/review/ReviewStarsContainer';
 import '../../../_styles/main.css';
