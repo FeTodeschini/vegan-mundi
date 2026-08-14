2f8a1c0804a22d14227076c01344f8394aa3804e
Felipe Todeschini <phelipe.tod@gmail.com>
2026-08-14 10:37:33 -0400

    #112-Added jwtSecret to config.js

--- server/config.js
+++ server/config.js
@@ -4,4 +4,5 @@ module.exports = {
     dbPort: process.env.DB_PORT?.trim(),
     dbUserName: process.env.DB_USER?.trim(),
     dbSecret: process.env.DB_SECRET?.trim(),
+    jwtSecretKey: process.env.JWT_SECRET_KEY?.trim(),
 }
